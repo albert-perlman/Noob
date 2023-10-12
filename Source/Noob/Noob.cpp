7b79b879bf4e6df9088ab4a605d44f53b069b61f
Albert Perlman <albert.perlman.dev@gmail.com>
2023-10-12 01:25:45 -0600

    Third Person template

---
+++ Source/Noob/Noob.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Noob.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Noob, "Noob" );
+ 
